a748b6835d8b3c2870108955e210094dabb053b6
Diomedes1990 <diomedes_frias@hotmail.com>
2016-08-20 10:14:26 -0400

    Add files via upload

---
+++ practica_3.sql
@@ -0,0 +1,70 @@
+﻿/*1.- Hacer un reporte que muestre el c�digo del empleado, nombre, supervisor (reportsto),
+y la cantidad de ordenes por mes que el empleado ha
+realizado por a�o (1996,1997,1998)*/
+
+SELECT emp.empid AS "Codigo del Empleado", emp.FirstName AS Nombre ,emp2.FirstName AS "Supervisor",
+	SUM(CASE WHEN YEAR(ord.OrderDate) = 1996 then 1 END ) as "1996",
+	SUM(CASE WHEN YEAR(ord.OrderDate) = 1997 then 1 END ) as "1997",
+	SUM(CASE WHEN YEAR(ord.OrderDate) = 1998 then 1 END ) as "1998",
+	COUNT(ord.OrderDate) AS "Cantidad de Ordenes" FROM HR.Employees AS emp
+	INNER JOIN SALES.Orders AS ord ON emp.empid = ord.empid
+	INNER JOIN HR.Employees AS emp2 ON emp.mgrid = emp2.empid
+WHERE YEAR(ord.OrderDate) IN (1996,1997,1998)
+GROUP BY emp.empid, emp.FirstName, emp.mgrid, emp2.FirstName
+
+
+/*2) -Hacer una consulta que liste c�digo, nombre, apellido del empleado y el numero y fecha de cada
+orden que se han realizado. Al final el result set debe verse as�:*/
+
+SELECT
+	HR.Employees.empid AS 'Nombre',
+	HR.Employees.firstname as 'Apellido',
+	HR.Employees.lastname as 'Empleado',
+	Orders.orderid AS 'Numero de Orden',
+	Orders.orderdate as 'Fecha'
+FROM
+	SALES.Orders
+	INNER JOIN HR.Employees 
+ON Orders.empid=hr.Employees.empid
+
+
+/*3) Crear un informe que muestra el ID del pedido, el nombre de la empresa que realiz� el pedido ,
+y el nombre y apellido del trabajador asociado .
+Mostrar s�lo los pedidos realizados a 1 de enero de 1998, que se enviaron despu�s de que fueran necesarios .
+Ordenar por Nombre de la empresa. El result set debe verse as�.*/
+
+SELECT
+	Orders.orderid AS 'Numero de Orden',
+	Sales.Customers.companyname AS 'Nombre Empresa',
+	HR.Employees.firstname as 'Empleado Asociado'
+FROM
+SALES.Orders
+INNER JOIN Sales.Customers ON Orders.custid=Sales.Customers.custid
+INNER JOIN HR.Employees ON ORDERS.empid=HR.Employees.empid
+where orderdate > '1998-01-01' and Orders.shippeddate > Orders.requireddate 
+Order by
+Sales.Customers.companyname
+
+
+
+/*4) Cree un reporte que muestre el n�mero de empleados y clientes de cada ciudad que tiene empleados
+en ella. El result set debe ser parecido a este:*/
+
+
+SELECT q1.numEmployees, q2.numCompanies, q1.City , q2.City   FROM 
+(SELECT COUNT(emp.City) AS numEmployees, emp.City AS City  FROM Hr.Employees AS emp GROUP BY emp.City)AS q1
+INNER  JOIN 
+(SELECT  COUNT(cus.City) AS numCompanies, cus.City AS City  FROM SALES.Customers AS cus GROUP BY cus.City)AS q2
+ON q1.City = q2.City ORDER BY q2.numCompanies DESC
+
+
+/*5) Cree un reporte que muestre en un solo result set, el nombre de contacto y numero de tel�fono de
+todos los empleados, clientes y suplidores.*/
+
+
+SELECT CAST(Hr.Employees.FirstName AS varchar)+SPACE(1)+CAST(Hr.Employees.LastName AS varchar) AS ContactName, Hr.Employees.phone AS Phone FROM HR.Employees
+UNION
+SELECT Sales.Customers.contactname, Sales.Customers.phone FROM SALES.Customers
+UNION
+SELECT Production.Suppliers.contactname, Production.Suppliers.phone FROM Production.Suppliers
+
---
+++ practica_4.sql
@@ -0,0 +1,76 @@
+﻿
+/*1)  Hacer un estore procedure que reciba dos par�metros [c�digo del empleado] y [opci�n] y dependiendo de la opci�n 1) lista 
+c�digo del empleado, nombre y apellido en may�sculas,titulo y fecha de contrataci�n, email (estar� formado por primera letra del 
+nombre + el apellido @ +primeras cinco letras del titulo + .com, ejemplo: Sara Davis, CEO, quedara: sdavis@ceo.com y la opci�n 2) 
+ser� listar c�digo del empleado, nombre y apellido en may�sculas, nombre completo del supervisor y su email. El par�metro numero de supervisor
+debe ser opcional*/
+
+CREATE PROCEDURE ejercicio1
+(
+@opcion char(2),
+@codigo_empleado varchar(1) = NULL
+)
+
+AS 
+
+BEGIN
+    --PARA MOSTRAR INFORMACION DE TODOS LOS EMPLEADOS 
+	IF (@opcion = 'IE') 
+	BEGIN
+		SELECT EmployeeID AS "Codigo Empleado", 
+		UPPER(FirstName) AS Nombre, 
+		UPPER(LastName) AS Apellido, 
+		Title AS Titulo, 
+		CONVERT(varchar, HireDate,103)  AS "Fecha Contratacion",
+		LOWER(LEFT(FirstName,1)+LastName+'@'+RTRIM(LEFT(Title,5))+'.com')  AS "E-mail" 
+		FROM Employees  
+	END
+
+	--PARA MOSTRAR INFORMACION DE UN EMPLEADO Y DE SU SUPERVISOR
+	IF (@opcion = 'IES') 
+	BEGIN
+		IF (ISNULL(@codigo_empleado,'')='')
+		BEGIN
+		RAISERROR('Por favor introdusca El Codigo Del Empleado',11,1)
+		END
+		SELECT emp1.EmployeeID AS "Codigo Empleado", 
+		UPPER(emp1.FirstName) AS Nombre, 
+		UPPER(emp1.LastName) AS Apellido,
+		emp2.FirstName+' '+emp2.LastName AS "Nombre Supervisor", 
+		LOWER(LEFT(emp2.FirstName,1)+emp2.LastName+'@'+RTRIM(LEFT(emp2.Title,5))+'.com')  AS "E-mail" 
+		FROM Employees AS emp1
+		INNER JOIN Employees AS emp2 ON emp1.ReportsTo = emp2.EmployeeID WHERE emp1.EmployeeID = CAST(@codigo_empleado AS INT) 
+	END
+
+END
+
+
+/*2)  Hacer un store procedure que muestre las ventas por vendedor de un periodo, los par�metros ser�n fecha inicial y fecha final. El listado debe mostrar c�digo del empleado, nombre
+completo, total de ordenes del periodo*/
+
+CREATE PROCEDURE ejercicio2
+(
+@final_inicial date,
+@fecha_final date
+)
+
+AS 
+
+BEGIN
+    --VALIDADOR DE FECHA NO ESTE EN BLANCO NI NULO	
+	IF ((ISNULL(@final_inicial,'') = '') OR (ISNULL(@fecha_final,'') = '')) 
+	BEGIN
+	RAISERROR('Una De Las Fechas No Es Validad',11,1)
+	END
+	ELSE
+	BEGIN
+		SELECT emp.EmployeeID AS "Codigo Empleado", 
+		emp.FirstName+''+emp.LastName AS "Nombre Completo",
+		COUNT(ord.OrderID) AS "Total de Ordenes"    
+		FROM Employees AS emp
+		INNER JOIN Orders AS ord ON emp.EmployeeID = ord.EmployeeID
+		WHERE ord.OrderDate BETWEEN @final_inicial AND @fecha_final
+		GROUP BY emp.EmployeeID, emp.FirstName, emp.LastName 
+	END
+
+END
